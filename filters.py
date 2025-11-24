8171da0bbfca617c892f7c42239cb5c5eebe023c
Camille <lecamille@gmail.com>
2025-11-23 16:48:48 -0800

    OK

--- filters.py
+++ filters.py
@@ -2,13 +2,38 @@ import cv2
 import numpy
 import utils
 
+
 def strokeEdges(src, dst, blurKsize=7, edgeKsize=5):
+    # Pre-processing: Blur and grayscale image
     if blurKsize >= 3:
         blurredSrc = cv2.medianBlur(src, blurKsize)
+        graySrc = cv2.cvtColor(blurredSrc, cv2.COLOR_BGR2GRAY)
+    else:
+        # Otherwise, convert the image to graycale
         graySrc = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
 
-    channels = cv2.split(src)
+    cv2.Laplacian(graySrc, cv2.CV_8U, graySrc, ksize=edgeKsize)
     normalizedInverseAlpha = (1.0 / 255) * (255 - graySrc)
+    channels = cv2.split(src)
     for channel in channels:
         channel[:] = channel * normalizedInverseAlpha
     cv2.merge(channels, dst)
+
+
+class VConvolutionFilter(object):
+    """A filter that applies a convolution to V (or all of BGR)."""
+    def __init__(self, kernel):
+        self._kernel = kernel
+
+    def apply(self, src, dst):
+        """Apply the filter with a BGR or gray source/destination."""
+        cv2.filter2D(src, -1, self._kernel, dst)
+
+
+class SharpenFilter(VConvolutionFilter):
+    """A sharpen filter with a 1-pixel radius."""
+    def __init__(self):
+        kernel = numpy.array([[-1, -1, -1],
+                              [-1, 9, -1],
+                              [-1, -1, -1]])
+        VConvolutionFilter.__init__(self, kernel)
